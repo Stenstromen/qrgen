861c71db9703e5bda38942937eecfec02ca500bf
Stenstromen <104073740+Stenstromen@users.noreply.github.com>
2023-01-31 21:52:39 +0100

    Initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+import NavBar from "./Components/NavBar";
+import Home from "./Pages/Home";
 
 function App() {
+  const [isMobile, setIsMobile] = useState<boolean>(false);
+
+  useEffect(() => {
+    return window.addEventListener("resize", () => {
+      window.innerWidth < 425 ? setIsMobile(true) : setIsMobile(false);
+    });
+  }, []);
+
+  useEffect(() => {
+    return window.innerWidth < 425 ? setIsMobile(true) : setIsMobile(false);
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <BrowserRouter>
+        <NavBar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ src/Components/NavBar.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function NavBar() {
+    return (
+        <div>
+            <p>lol</p>
+        </div>
+    )
+}
+
+export default NavBar;
